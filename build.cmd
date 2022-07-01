54fa3c8ff3ce898c5ca122b02fe459092d73a060
Julian Easterling <julian@julianscorner.com>
2022-07-01 09:56:07 -0400

    Updated the local build script to use VERSION file as well.

--- build.cmd
+++ build.cmd
@@ -1,14 +1,15 @@
 @echo off
 setlocal
-set THTTPD_VERSION=2.29
 
-docker build --pull --progress plain -t dcjulian29/httpd:%THTTPD_VERSION% . ^
-  --build-arg THTTPD_VERSION=%THTTPD_VERSION%
+pushd %~dp0
+
+for /f "delims=" %%x in (version) do set THTTPD_VERSION=%%x
+
+docker build --build-arg THTTPD_VERSION=%THTTPD_VERSION% --progress plain ^
+  -t dcjulian29/httpd:%THTTPD_VERSION% .
+
+if %errorlevel% neq 0 popd;exit /b %errorlevel%
+
+popd
 
 docker tag dcjulian29/httpd:%THTTPD_VERSION%  dcjulian29/httpd:latest
-
-if "%1" == "" GOTO :EOF
-
-echo --------------------------------------------------------------------------
-
-docker push dcjulian29/httpd --all-tags
